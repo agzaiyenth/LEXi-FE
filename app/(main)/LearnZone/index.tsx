fbbed77be33d15e600c01f691e7d095645730464
Ashirwada Manamudali <zionashirwada@users.noreply.github.com>
2024-12-31 09:04:07 +0530

    init files

--- app/(main)/LearnZone/index.tsx
+++ app/(main)/LearnZone/index.tsx
@@ -1,12 +1,27 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+// app/(main)/LearnZone/index.tsx
+import React from 'react';
+import VoxBuddy from './VoxBuddy'; 
+import LearnMainScreen from './LearnMainScreen';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const LearnScreen = () => {
+export type LearnZoneParamList = {
+  LearnMain: undefined; // No parameters for the main screen
+  VoxBuddy: undefined; // No parameters for VoxBuddy
+};
+
+const Stack = createStackNavigator<LearnZoneParamList>();
+
+const LearnZone = () => {
   return (
-    <View>
-      <Text>LearnScreen</Text>
-    </View>
-  )
-}
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false, // Disable headers for a cleaner look
+      }}
+    >
+      <Stack.Screen name="LearnMain" component={LearnMainScreen} />
+      <Stack.Screen name="VoxBuddy" component={VoxBuddy} />
+    </Stack.Navigator>
+  );
+};
 
-export default LearnScreen
+export default LearnZone;
--- config.ts
+++ config.ts
@@ -1,1 +1,2 @@
-export const BASE_URL="http://10.0.2.2:8080"
+export const BASE_ENDPOINT=`10.0.2.2:8080`
+export const BASE_URL=`http://${BASE_ENDPOINT}`
