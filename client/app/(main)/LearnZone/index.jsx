e4f25ee8fbf061b2ec3a3dfbfc0c7b9d9678f405
Agzaiyenth Ganaraj <agzaiyenth2@gmail.com>
2025-01-05 23:41:05 +0530

    Update index.jsx

--- client/app/(main)/LearnZone/index.jsx
+++ client/app/(main)/LearnZone/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 
+
 const LearnScreen = () => {
   return (
     <View style={styles.wrapper}>
