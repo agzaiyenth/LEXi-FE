4b0dbcb42b26b3f37eaef9358a440e6fb7972f9b
Ashirwada Manamudali <zionashirwada@users.noreply.github.com>
2025-01-05 17:34:51 +0530

    add comments

--- client/app/(main)/LearnZone/LearnMainScreen.tsx
+++ client/app/(main)/LearnZone/LearnMainScreen.tsx
@@ -1,7 +1,13 @@
+// app/(main)/LearnZone/LearnMainScreen.tsx
+/* 
+This is the main screen for the LearnZone . 
+It contains buttons that navigate to other screens in the LearnZone feature. 
+*/
+
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { LearnZoneParamList } from './index'; // Import the param list
+import { LearnZoneParamList } from './index'; 
 import { StackNavigationProp } from '@react-navigation/stack';
 
 type LearnMainNavigationProp = StackNavigationProp<LearnZoneParamList, 'LearnMain'>;
@@ -14,8 +20,17 @@ const LearnMainScreen = () => {
       <Text style={styles.title}>Welcome to LearnZone</Text>
       <Button
         title="Go to Vox Buddy"
-        onPress={() => navigation.navigate('VoxBuddy')} // Type-safe navigation
+        onPress={() => navigation.navigate('VoxBuddy')} 
       />
+      <Button
+        title="Go to Smarty read"
+        onPress={() => navigation.navigate('VoxBuddy')}   // @AmandaHanz update this navigation
+      />
+      <Button
+        title="Go to read with me"
+        onPress={() => navigation.navigate('VoxBuddy')} // @asraameer update this navigation
+      />
+
     </View>
   );
 };
@@ -25,6 +40,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    gap: 20,
   },
   title: {
     fontSize: 20,
