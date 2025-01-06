2fd26b1292866cbe755baed4747122627e98e178
Amanda Hansamali <amandahansamali18@gmail.com>
2025-01-06 11:41:25 +0530

    onboarding smartread screen

---
+++ client/app/(main)/LearnZone/SmartRead.tsx
@@ -0,0 +1,19 @@
+// app/(main)/LearnZone/SmartRead.tsx
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const SmartRead = () => {
+  return (
+    <View style={styles.container}>
+     
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default SmartRead;
