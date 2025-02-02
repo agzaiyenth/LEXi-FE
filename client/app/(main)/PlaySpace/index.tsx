a0c9b484109c1b91d6faee1ee6656765554c56a7
Agzaiyenth Ganaraj <agzaiyenth2@gmail.com>
2025-02-02 23:19:26 +0530

    Refactor game structure by renaming and organizing components; add GameB and enhance BalloonGame UI with navigation and game details

--- client/app/(main)/PlaySpace/index.tsx
+++ client/app/(main)/PlaySpace/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import PlayMainScreen from './PlayMainScreen';
 import BalloonGame from './BalloonGame';
+import GameB from './GameB';
 
 export type LearnZoneParamList = {
   PlayMain: undefined; 
   BalloonGame: undefined; 
+  GameB: undefined;
 };
 
 const Stack = createStackNavigator<LearnZoneParamList>();
@@ -19,6 +21,7 @@ const PlaySpace = () => {
     >
       <Stack.Screen name="PlayMain" component={PlayMainScreen} />
       <Stack.Screen name="BalloonGame" component={BalloonGame} />
+      <Stack.Screen name="GameB" component={GameB} />
     </Stack.Navigator>
   );
 };
