c14842f4a81f4669d75107abe9738676ffdabb6a
Ashirwada Manamudali <zionashirwada@users.noreply.github.com>
2025-02-16 14:12:04 +0530

    feat: enhance LoadingScreen with image and improved styling for better user experience

--- client/src/components/loading.tsx
+++ client/src/components/loading.tsx
@@ -1,12 +1,39 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { View, Text, StyleSheet, ActivityIndicator, Image } from 'react-native'
+import theme from '../theme'
 
 const LoadingScreen = () => {
   return (
-    <View>
-      <Text>loading</Text>
+    <View style={styles.container}>
+      <Image
+        source={{ uri: 'https://via.placeholder.com/150' }}
+        style={styles.image}
+      />
+      <ActivityIndicator size="large" color={theme.colors.primary.dark2} style={styles.indicator} />
+      <Text style={styles.loadingText}>Loading...</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.colors.background.offWhite,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: 150,
+    height: 150,
+    marginBottom: 20,
+  },
+  indicator: {
+    marginBottom: 10,
+  },
+  loadingText: {
+    fontSize: 18,
+    color: '#333',
+  },
+})
 
 export default LoadingScreen
