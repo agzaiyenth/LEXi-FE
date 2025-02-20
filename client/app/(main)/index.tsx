d8117ae7c7af72aed67acf8205735212b4a69c52
Ashirwada Manamudali <91813126+zionashirwada@users.noreply.github.com>
2025-02-20 19:02:28 +0530

    Improvement /improve home (#42)
    * update blog cards
    
    * add crosel
    
    * update image source for PlaySpace and add keys to Animated.View components in BlogCard and FeatureCard
    
    * update images
    
    * update car
    
    * Update index.tsx
    
    ---------
    
    Co-authored-by: Ashirwada Manamudali <zionashirwada@users.noreply.github.com>

--- client/app/(main)/index.tsx
+++ client/app/(main)/index.tsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import HomeScreen from './Home'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 export class Index extends Component {
   render() {
     return (
+      <GestureHandlerRootView style={{ flex: 1 }}>
       <HomeScreen/>
+      </GestureHandlerRootView>
     )
   }
 }
