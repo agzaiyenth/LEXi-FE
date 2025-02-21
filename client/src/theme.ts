699d4c0c226b4bcd2f5aa247d1bebacb7cc7d90b
asra-ameer <151917440+asra-ameer@users.noreply.github.com>
2025-02-21 09:46:24 +0530

    update theme.ts

--- client/src/theme.ts
+++ client/src/theme.ts
@@ -1,48 +1,61 @@
 // theme.ts
-export  const theme = {
+export const theme = {
   colors: {
     primary: {
-      dark1: '#264653',  // Darkest shade
-      dark2: '#254d4e',
-      dark3:'#03767a',  // Darker shade
-      medium: '#2A9D8F', // Medium shade
-      medium2:'#98c1b9', // Medium shade
-      light: '#A8DADC',  // Lighter shade
-      light2: '#D3E0DE',
-      light3:'#9ac3bb',
+      dark1: "#264653", // Darkest shade
+      dark2: "#254d4e",
+      dark3: "#03767a", // Darker shade
+      medium: "#2A9D8F", // Medium shade
+      medium2: "#98c1b9", // Medium shade
+      light: "#A8DADC", // Lighter shade
+      light2: "#D3E0DE",
+      light3: "#9ac3bb",
     },
     secondary: {
-      dark1: '#A17E56',
-      dark2: '#',
-      medium: '#F4A261',
-      light: '#',
-      light2: '#fef8ea',
+      dark1: "#A17E56",
+      dark2: "#",
+      medium: "#F4A261",
+      light: "#",
+      light2: "#fef8ea",
     },
     accent: {
-      dark: '#6D597A',
-      medium: '#B56576',
-      light: '#E56B6F',
+      dark: "#6D597A",
+      medium: "#B56576",
+      light: "#E56B6F",
     },
     background: {
-      offWhite: '#FFF9EB', // Off-white
-      beige: '#F8ECD1', // Beige
-      dark: '#D9CAB3',   // Darker beige
+      offWhite: "#FFF9EB", // Off-white
+      beige: "#F8ECD1", // Beige
+      dark: "#D9CAB3", // Darker beige
     },
-    blacks:{
-      medium:'#151717',
-      dark:'#000000'
-
+    blacks: {
+      medium: "#151717",
+      dark: "#000000",
     },
   },
   fonts: {
-    regular: 'System',
-    regular2: 'OpenDyslexic',
-    bold: 'System',
+    regular: "System",
+    regular2: "OpenDyslexic",
+    bold: "System",
     sizes: {
       small: 16,
       medium: 20,
       large: 30,
       extraLarge: 40,
+      14: 14,
+      16: 16,
+      18: 18,
+      20: 20,
+      22: 22,
+      24: 24,
+      26: 26,
+      28: 28,
+      30: 30,
+      32: 32,
+      34: 34,
+      36: 36,
+      38: 38,
+      40: 40,
     },
   },
   spacing: {
@@ -50,6 +63,20 @@ export  const theme = {
     medium: 16,
     large: 24,
     extraLarge: 32,
+    14: 14,
+    16: 16,
+    18: 18,
+    20: 20,
+    22: 22,
+    24: 24,
+    26: 26,
+    28: 28,
+    30: 30,
+    32: 32,
+    34: 34,
+    36: 36,
+    38: 38,
+    40: 40,
   },
   accessibility: {
     highContrast: false,
