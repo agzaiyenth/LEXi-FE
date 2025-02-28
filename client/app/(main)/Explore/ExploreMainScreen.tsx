615e34a14a23f71a230ff95b45589378f3754900
Rikas <128444448+rikas-ilamdeen@users.noreply.github.com>
2025-02-28 17:58:43 +0530

    update Explore page

--- client/app/(main)/Explore/ExploreMainScreen.tsx
+++ client/app/(main)/Explore/ExploreMainScreen.tsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { StyleSheet, View } from 'react-native'
 import TherapistHome from './TherapistHome'
+import { useTheme } from "../../../src/context/ThemeContext";
 
 export default function ExploreMainScreen() {
+  const { theme } = useTheme();
+
+  const styles = useMemo(() =>
+    StyleSheet.create({}), [theme]
+  );
+
   return (
     <View>
       <TherapistHome />
     </View>
   )
 }
-
-const styles = StyleSheet.create({})
