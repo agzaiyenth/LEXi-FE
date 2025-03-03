6ee28714252c994d0fd7b938f54d3b729ad99058
Ashirwada Manamudali <zionashirwada@users.noreply.github.com>
2025-03-03 09:03:35 +0530

    Update metro.config.js

--- client/metro.config.js
+++ client/metro.config.js
@@ -1,14 +1,23 @@
+// metro.config.js
 const { getDefaultConfig } = require("@expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
+// Get the default Expo config
 const config = getDefaultConfig(__dirname);
 
-// Fix Metro bundler waiting issue
+// If not already defined, define resolver
 config.resolver = config.resolver || {};
-config.resolver.blockList = [/\/android\/.*/]; // Prevent infinite looping in Android folder
 
-// Ensure SVGs are processed correctly
+// 1) Block the android directory (prevents scanning)
+const exclusionList = require("metro-config/src/defaults/exclusionList");
+config.resolver.blockList = exclusionList([
+  // This regex will block anything inside /android/.
+  /\/android\/.*/,
+]);
+
+// 2) Ensure handling of .svg files if you're using `react-native-svg-transformer`:
 config.resolver.assetExts = config.resolver.assetExts.filter((ext) => ext !== "svg");
 config.resolver.sourceExts = [...config.resolver.sourceExts, "svg"];
 
+// 3) If using NativeWind:
 module.exports = withNativeWind(config, { input: "./global.css" });
