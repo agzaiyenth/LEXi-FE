cc44470d47aa9c3f551ad317a2b7475e14db9264
Ashirwada Manamudali <zionashirwada@users.noreply.github.com>
2025-03-07 06:55:57 +0530

    update base

--- client/config.ts
+++ client/config.ts
@@ -1,2 +1,3 @@
-export const BASE_ENDPOINT = process.env.ENV_BASE_ENDPOINT || `10.0.2.2:8080`;
+export const BASE_ENDPOINT = process.env.ENV_BASE_ENDPOINT || `be-lexi-dpgth7c7feauczh8.southeastasia-01.azurewebsites.net`;
 export const BASE_URL=`http://${BASE_ENDPOINT}`
+    
