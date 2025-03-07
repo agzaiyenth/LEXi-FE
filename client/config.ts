2fa8f5c628698f5a6f985ca4d31bca10bda09da6
Ashirwada Manamudali <zionashirwada@users.noreply.github.com>
2025-03-07 08:33:38 +0530

    Update config.ts

--- client/config.ts
+++ client/config.ts
@@ -1,3 +1,3 @@
-export const BASE_ENDPOINT = process.env.ENV_BASE_ENDPOINT || `10.0.2.2:8080`;
-export const BASE_URL=`http://${BASE_ENDPOINT}`
-    
+export const BASE_ENDPOINT = `10.0.2.2:8080`;
+export const BASE_URL=`https://${BASE_ENDPOINT}`
+export const BASE_WS_URL=`wss://${BASE_ENDPOINT}`
